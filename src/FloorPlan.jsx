cca3ed583212a9ff9764fac18a268ae5a67eedc9
Ryan Combest <crcombest@gmail.com>
2022-10-10 19:03:38 -0500

    completes lab

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,12 +1,18 @@
 import Bedroom from "./Bedroom"
+import FullBath from "./FullBath"
+import HalfBath from "./HalfBath"
 import Kitchen from "./Kitchen"
+import LivingRoom from "./LivingRoom"
 
 function FloorPlan(props) {
   return (
     <>
     <Bedroom bedNum={1}/>
     <Kitchen />
+    <FullBath />
     <Bedroom bedNum={2}/>
+    <LivingRoom />
+    <HalfBath />
     <Bedroom bedNum={3}/>
 
     </>
